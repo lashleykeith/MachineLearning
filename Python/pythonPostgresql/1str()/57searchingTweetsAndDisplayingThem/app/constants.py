920409afa0bf0a2aa344c1e98ca81e14c1f24162
Keith Lashley <lashleykeith@gmail.com>
2022-10-17 00:39:31 +0900

    Add files via upload

---
+++ Python/pythonPostgresql/1str()/57searchingTweetsAndDisplayingThem/app/constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = 'ElfXDhQA8n3LG37gZ7Ihgsbwd'
+CONSUMER_SECRET = 'aJ9XrlNvZcNxugFSy1bd6FE52aykhsfSOAI3idqDX9UlZgMEhM'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
---
+++ Python/pythonPostgresql/1str()/57searchingTweetsAndDisplayingThem/projects/app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, session, redirect, request, url_for, g
+from twitter_utils import get_request_token, get_oauth_verifier_url, get_access_token
+from user import User
+from database import Database
+
+app = Flask(__name__)
+app.secret_key = 'flashlantern'
+
+Database.initialise(host='localhost', database='learning1', user='postgres', password='flashlantern')
+
+@app.before_request
+def load_user():
+    if 'screen_name' in session:
+        g.user = User.load_from_db_by_screen_name(session['screen_name'])
+
+@app.route('/')
+def homepage():
+   # return "Hello, world!"
+    return render_template('home.html')
+
+@app.route('/login/twitter')
+def twitter_login():
+    if 'screen_name' in session:
+        return redirect(url_for('profile'))
+    request_token = get_request_token()
+    session['request_token'] = request_token
+
+    #return redirect('http://example.com')
+    return redirect(get_oauth_verifier_url(request_token))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('homepage'))
+
+@app.route('/auth/twitter') # http://127.0.0.1:4995/auth/twitter?oauth_verifier=1234567
+def twitter_auth():
+    oauth_verifier = request.args.get('oauth_verifier')
+    access_token = get_access_token(session['request_token'], oauth_verifier)
+
+    user = User.load_from_db_by_screen_name(access_token['screen_name'])
+    if not user:
+        user = User(access_token['screen_name'], access_token['oauth_token'],
+                    access_token['oauth_token_secret'], id)
+        user.save_to_db()
+
+    session['screen_name'] = user.screen_name
+
+    return redirect(url_for('profile'))
+    #return redirect('http://127.0.0.1:4995/profile')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html',user = g.user)
+   # return render_template('profile.html',user = User.load_from_db_by_screen_name(session['screen_name']))
+
+@app.route('/search')
+def search():
+    tweets = g.user.twitter_request('https://api.twitter.com/1.1/search/tweets.json?q=pythonjobs+filter:images')
+
+    tweet_texts = [tweet['text'] for tweet in tweets['statuses']]
+
+    return render_template('search.html', content=tweet_texts)
+
+app.run(port=4995, debug=True)
+
+
+
+
+
+    # GET http://127.0.0.1:4996/users
+#http://127.0.0.1:4995/auth/twitter
