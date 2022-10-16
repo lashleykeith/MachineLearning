a109809e91448258ccd73f367a35a011bbd6188a
Keith Lashley <lashleykeith@gmail.com>
2022-10-16 19:43:49 +0900

    Add files via upload

---
+++ Python/megacourse/1Variables/Script.py
@@ -0,0 +1,19 @@
+print("Hey there")
+print("Ardit".replace('A','O'))
+
+replaced='Ardit'.replace('A','O')
+print(replaced)
+currency=145
+currency_today=100.5
+currency1='nothing'
+
+
+student='John '
+action1 = 'went to the '
+action2 = 'will go to the '
+action3 = 'is going to the '
+place1 = 'mall'
+place2 = 'book store'
+print(currency,currency_today,currency1)
+
+print(student,action1,place1)
