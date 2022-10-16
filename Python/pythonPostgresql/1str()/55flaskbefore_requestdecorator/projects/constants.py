4ce03ae04d1fbc8b2df93bdef5201986aa4a800c
Keith Lashley <lashleykeith@gmail.com>
2022-10-17 00:37:45 +0900

    Add files via upload

---
+++ Python/pythonPostgresql/1str()/55flaskbefore_requestdecorator/projects/constants.py
@@ -0,0 +1,7 @@
+CONSUMER_KEY = 'DFjVAxuuyyJbkwCWvPHVdyn8Y'
+CONSUMER_SECRET = 'hAcYARewmRE6NPFwdm4IhrFmMlPJ4P1SOpMbV0PbutYHBsBx7m'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
+#0.10.1 ,0.12.2
