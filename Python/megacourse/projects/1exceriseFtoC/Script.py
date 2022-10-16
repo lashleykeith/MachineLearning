303c36614ac6e530e49795d0fdf78b4996603fe8
Keith Lashley <lashleykeith@gmail.com>
2022-10-16 19:48:50 +0900

    Add files via upload

---
+++ Python/megacourse/projects/1exceriseFtoC/Script.py
@@ -0,0 +1,5 @@
+#Please create a function that converts Celsius degrees to Fahreheit.
+def c_to_f(c):
+	f=c*9/5+32
+	return f
+print(c_to_f(1))
---
+++ Python/megacourse/projects/2moneyarraydictionary/Script.py
@@ -0,0 +1,7 @@
+money={"saving_account":200,"checking_account":100, "under_bed":[500,10,100]}
+
+
+money["under_bed"][1]
+
+print(money["under_bed"][1])
+print(money["under_bed"][2])
